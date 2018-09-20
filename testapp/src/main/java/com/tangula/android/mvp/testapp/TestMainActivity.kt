77123b416331a79431cdbebfa2b7fcdefcf19025
临碣 <wangds2016@163.com>
2018-09-20 11:00:07 +0800

    add test app

---
+++ testapp/src/main/java/com/tangula/android/mvp/testapp/TestMainActivity.kt
@@ -0,0 +1,12 @@
+package com.tangula.android.mvp.testapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TestMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_main)
+    }
+}
