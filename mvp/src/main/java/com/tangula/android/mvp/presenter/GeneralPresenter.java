452063173faebb2ac7a8ff3506953f87d15900d4
临碣 <wangds2016@163.com>
2018-09-25 11:26:46 +0800

    代码升级为1.8

--- mvp/src/main/java/com/tangula/android/mvp/presenter/GeneralPresenter.java
+++ mvp/src/main/java/com/tangula/android/mvp/presenter/GeneralPresenter.java
@@ -1,7 +1,6 @@
 package com.tangula.android.mvp.presenter;
 
 import android.annotation.SuppressLint;
-import android.os.AsyncTask;
 
 import com.tangula.android.mvp.module.Module;
 import com.tangula.android.utils.UiThreadUtils;
@@ -10,10 +9,11 @@ import com.tangula.utils.function.Supplier;
 
 /**
  * 通用Presenter.
+ *
  * @param <V> ViewHolder类型.
  * @param <M> 视图模型类型.
  */
-public abstract class GeneralPresenter<V extends Presenter.ViewHolder,M extends Module> implements Presenter<V,M> {
+public abstract class GeneralPresenter<V extends Presenter.ViewHolder, M extends Module> implements Presenter<V, M> {
 
     private V mViewHolder;
 
@@ -21,9 +21,10 @@ public abstract class GeneralPresenter<V extends Presenter.ViewHolder,M extends 
 
     /**
      * 构造函数．
-     * @param vhFac　生成ViewHolder的工厂方法．
+     *
+     * @param vhFac 　生成ViewHolder的工厂方法．
      */
-    protected GeneralPresenter(Supplier<V> vhFac){
+    protected GeneralPresenter(Supplier<V> vhFac) {
         this.mViewHolder = vhFac.get();
     }
 
@@ -48,41 +49,20 @@ public abstract class GeneralPresenter<V extends Presenter.ViewHolder,M extends 
      * <p>异步执行　{@link #loadModule(Consumer)} 获取模型对象,执行 {@link #onRefresh()}刷新视图中的数据.  </p>
      */
     @SuppressLint("StaticFieldLeak")
-    public void refresh(){
-
-        new AsyncTask<Object, Object, Object>(){
-            @Override
-            protected Object doInBackground(Object... objects) {
-                loadModule(new Consumer<M>() {
-                    @Override
-                    public void accept(M m) {
-                        setModule(m);
-
-                        V vh = getViewHolder();
-
-                        new AsyncTask(){
-                            @Override
-                            protected Object doInBackground(Object... objects) {
-                                return null;
-                            }
-                            @Override
-                            protected void onPostExecute(Object o) {
-                                onRefresh();
-                            }
-                        }.execute();
-                    }
-                });
-                return null;
-            }
-        }.execute();
-
-
+    public void refresh() {
+        //在后台线程执行加载数据过程
+        UiThreadUtils.runInBackground(() -> loadModule(m -> {
+            setModule(m);
+            //在UI线程中执行刷新页面的操作
+            UiThreadUtils.runInUiThread(this::refresh);
+        }));
 
     }
 
     /**
      * 更新Module数据.
      * <p>实现的时候，在获得新的Module数据之后，调用callbackLoadResultHandler.accept(新的module)</p>
+     *
      * @param callbackLoadResultHandler 更新后的新数据的回调函数.
      */
     protected abstract void loadModule(Consumer<M> callbackLoadResultHandler);
